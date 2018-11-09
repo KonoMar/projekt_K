7bf334e2ab29fad2b99dd63f084926df76dbde3a
MikeW210 <44272309+MikeW210@users.noreply.github.com>
2018-11-09 16:28:28 +0100

    wszystko z konfiguracji

--- src/main/java/pl/dmcs/projektkompetencyjny/ProjektkompetencyjnyApplication.java
+++ src/main/java/pl/dmcs/projektkompetencyjny/ProjektkompetencyjnyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ImportResource;
 
 
 @SpringBootApplication
-@ImportResource("classpath:config/spring-config.xml")
+
 public class ProjektkompetencyjnyApplication {
 
 	public static void main(String[] args) {
