93107293289aec36aca788cf2ecc233e4302b228
MikeW210 <44272309+MikeW210@users.noreply.github.com>
2018-11-14 10:58:27 +0100

    wszystko do web Scopes dodawanie problem z podniana rycerza a nie dodatniem do listy

--- src/main/java/pl/dmcs/projektkompetencyjny/controllers/KnightController.java
+++ src/main/java/pl/dmcs/projektkompetencyjny/controllers/KnightController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import pl.dmcs.projektkompetencyjny.components.TimeComponent;
 import pl.dmcs.projektkompetencyjny.domain.Knight;
+import pl.dmcs.projektkompetencyjny.domain.PlayerInformation;
 import pl.dmcs.projektkompetencyjny.domain.repository.KnightRepository;
 import pl.dmcs.projektkompetencyjny.services.KnightService;
 
@@ -19,24 +21,37 @@ import java.util.List;
 public class KnightController {
 
     @Autowired
+    TimeComponent timeComponent;
+
+    @Autowired
+    PlayerInformation playerInformation;
+
+    @Autowired
     KnightService service;
 
     @RequestMapping("/knights")
     public String getKnights(Model model){
         List<Knight> allKnights = service.getAllKnights();
         model.addAttribute("knights",allKnights);
+        model.addAttribute("timecomponent",timeComponent);
+        model.addAttribute("playerInformation",playerInformation);
     return "knights";
     }
     @RequestMapping("/knight")
     public String getKnights(@RequestParam("id") Integer id, Model model){
         Knight knight = service.getKnight(id);
         model.addAttribute("knight", knight);
+        model.addAttribute("timecomponent",timeComponent);
+        model.addAttribute("playerInformation",playerInformation);
+
         return "knight";
     }
 
     @RequestMapping("/newknight")
     public String createKnight (Model model){
         model.addAttribute("knight",new Knight());
+        model.addAttribute("timecomponent",timeComponent);
+        model.addAttribute("playerInformation",playerInformation);
         return "knightform";
     }
     @RequestMapping(value = "/knights", method = RequestMethod.POST)
